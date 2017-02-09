a3af281079dd2d8ce7b9cde7c0ba06b13298c32c
Iakov Salikov <salikovpro@gmail.com>
2017-02-09 21:52:13 +0500

    Fix: dev environment in store

--- src/entries/demo/store.js
+++ src/entries/demo/store.js
@@ -2,18 +2,17 @@ import { applyMiddleware, createStore, combineReducers } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import { routerReducer } from 'react-router-redux'
 import thunk from 'redux-thunk'
-import { debug } from 'libs/utils'
 import * as reducers from './reducers'
 
 /** Appliction middlewares */
 const middlewares = [thunk, promiseMiddleware()];
 
 /** Debug middlewares */
-if (debug) {
+if (process.env.NODE_ENV !== 'production') {
     middlewares.push(require('redux-logger')());
 }
 
 export default createStore(combineReducers({
     ...reducers,
     routing: routerReducer
-}), applyMiddleware.apply(this, middlewares));
+}), applyMiddleware(...middlewares));
